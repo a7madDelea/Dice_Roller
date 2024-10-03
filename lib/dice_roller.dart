d63d12520096cd8ea4b09761570ced045fcd3bd1
Ahmad Nidal Delea <a7mad.delea@gmail.com>
2024-10-03 03:32:47 -0700

    Dice Roller Create and activate the roll dice button.

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,18 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DiceRoller extends StatelessWidget {
+final Random random = Random();
+
+class DiceRoller extends StatefulWidget {
   const DiceRoller({
     super.key,
   });
+
+  @override
+  State<DiceRoller> createState() => _DiceRollerState();
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  int activeDice = 1;
+
+  void _roolDice() {
+    setState(() {
+      activeDice = random.nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
+        // Image.
         Image.asset(
-          'images/dice-1.png',
+          'images/dice-$activeDice.png',
           width: 250,
+        ),
+        const SizedBox(height: 50),
+        // Button
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onPressed: _roolDice,
+          child: const Text('Roll Dice'),
         ),
       ],
     );
